a42718f84be1062460416789171d04e60a0cd5c4
diegolins10 <dede140126>
2022-03-06 02:42:32 -0300

    Criando uma model user que representa o usuario

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using ApiAuth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAuth.Repositories
+{
+    public class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+            users.Add(new User { Id = 1, Username = "batman", Password = "batman", Role = "manager" });
+            users.Add(new User { Id = 2, Username = "robin", Password = "robin", Role = "employee" });
+
+
+            return users
+                   .FirstOrDefault(x =>
+                   x.Username.ToLower() == username.ToLower() && x.Password == password);
+        }
+    }
+}
